e567ae0734c88c188289cea00da56603b7198e0d
Dai Nguyen <ncdai3651408@gmail.com>
2022-02-07 12:04:06 +0700

    v2.4: new font (Open Sans), improve card.admin, improve card.file, ...

--- pc-dist/assets/js/za-dark-pc.darwin.js
+++ pc-dist/assets/js/za-dark-pc.darwin.js
@@ -4,5 +4,9 @@
   Made by NCDAi Studio
 */
 
+document.getElementsByTagName('head')[0].insertAdjacentHTML(
+  'beforeend',
+  `<style>@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap');</style>`
+);
 document.body.classList.add("za-dark", "za-dark-pc", "za-dark-darwin");
 document.documentElement.setAttribute("data-theme", "dark");
