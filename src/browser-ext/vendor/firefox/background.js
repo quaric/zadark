4f0115299e6d2f439f9351b87491752200c3dd73
Dai Nguyen <ncdai3651408@gmail.com>
2022-02-19 15:13:11 +0700

    Changed: firefox/background.js

--- src/browser-ext/vendor/firefox/background.js
+++ src/browser-ext/vendor/firefox/background.js
@@ -6,10 +6,10 @@
 
 browser.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
-    chrome.tabs.create({ url: 'welcome.html' })
+    browser.tabs.create({ url: 'welcome.html' })
   }
 
   if (details.reason === 'update') {
-    chrome.tabs.create({ url: 'changelog.html' })
+    browser.tabs.create({ url: 'changelog.html' })
   }
 })
