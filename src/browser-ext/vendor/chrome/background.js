05761fda94394d7f8501807f39d8f235ec33200b
Dai Nguyen <ncdai3651408@gmail.com>
2022-04-11 19:53:44 +0700

    Chrome: add contextMenu

--- src/browser-ext/vendor/chrome/background.js
+++ src/browser-ext/vendor/chrome/background.js
@@ -4,6 +4,33 @@
   Made by NCDAi Studio
 */
 
+const menus = [
+  {
+    id: 'help',
+    title: 'Trợ giúp',
+    contexts: ['action'],
+    onClick: () => {
+      chrome.tabs.create({ url: 'https://zadark.ncdaistudio.com/contact' })
+    }
+  },
+  {
+    id: 'donate',
+    title: 'Ủng hộ ZaDark',
+    contexts: ['action'],
+    onClick: () => {
+      chrome.tabs.create({ url: 'https://zadark.ncdaistudio.com/donate' })
+    }
+  },
+  {
+    id: 'rate',
+    title: 'Đánh giá trên Chrome Web Store',
+    contexts: ['action'],
+    onClick: () => {
+      chrome.tabs.create({ url: 'https://chrome.google.com/webstore/detail/llfhpkkeljlgnjgkholeppfnepmjppob' })
+    }
+  }
+]
+
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     chrome.tabs.create({ url: 'https://zadark.ncdaistudio.com/browser-ext/chrome' })
@@ -15,5 +42,20 @@ chrome.runtime.onInstalled.addListener((details) => {
         chrome.tabs.create({ url: 'changelog.html' })
       }
     })
+  }
+
+  menus.forEach((menu) => {
+    chrome.contextMenus.create({
+      id: menu.id,
+      title: menu.title,
+      contexts: menu.contexts
+    })
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const menu = menus.find((menu) => menu.id === info.menuItemId)
+  if (menu) {
+    menu.onClick()
   }
 })
