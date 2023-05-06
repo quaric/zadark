23f00428d47e68e64f2ea92d581cd7f982d7bc2c
Nguyễn Chánh Đại <ncdai@penphy.edu.vn>
2023-05-06 22:27:25 +0700

    changed(hide-thread-chat-message): improve ui/ux; added(tippy)

--- src/pc/assets/js/zadark.js
+++ src/pc/assets/js/zadark.js
@@ -199,6 +199,12 @@ async function handleHideThreadChatMessageChange () {
   window.zadark.utils.refreshHideThreadChatMessage()
 }
 
+const iconQuestionSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="14" height="14">
+    <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z" fill="currentColor" />
+  </svg>
+`
+
 const zadarkButtonHTML = `
   <div id="div_Main_TabZaDark" class="clickable leftbar-tab flx flx-col flx-al-c flx-center rel" data-id="div_Main_TabZaDark" data-translate-title="STR_MENU_ZADARK" title="ZaDark">
     <svg width="28" height="28" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -288,7 +294,10 @@ const popupMainHTML = `
         <div class="zadark-panel__body">
           <div class="zadark-switch__list">
             <div class="zadark-switch">
-              <label class="zadark-switch__label" for="js-switch-hide-latest-message">Ẩn "Tin nhắn gần nhất" trong Danh sách trò chuyện</label>
+              <label class="zadark-switch__label zadark-switch__label--helper" for="js-switch-hide-latest-message">
+                Ẩn "Tin nhắn gần nhất" trong Danh sách trò chuyện
+                <span class="zadark-switch__label--helper-icon" data-tippy-content="Khi kích hoạt, nội dung tin nhắn gần nhất trong danh sách trò chuyện (bên trái) sẽ được ẩn để ngăn chặn người khác nhìn trộm tin nhắn. Để xem nội dung, bạn rê chuột hoặc nhấn vào cuộc trò chuyện."></span>
+              </label>
               <label class="zadark-switch__checkbox">
                 <input class="zadark-switch__input" type="checkbox" id="js-switch-hide-latest-message">
                 <span class="zadark-switch__slider"></span>
@@ -296,7 +305,10 @@ const popupMainHTML = `
             </div>
 
             <div class="zadark-switch">
-              <label class="zadark-switch__label" for="js-switch-hide-thread-chat-message">Ẩn "Tin nhắn" trong Cuộc trò chuyện</label>
+              <label class="zadark-switch__label zadark-switch__label--helper" for="js-switch-hide-thread-chat-message">
+                Ẩn "Tin nhắn" trong Cuộc trò chuyện
+                <span class="zadark-switch__label--helper-icon" data-tippy-content="Khi kích hoạt, nội dung tin nhắn trong cuộc trò chuyện sẽ được làm mờ để ngăn chặn người khác nhìn trộm tin nhắn. Để xem nội dung, bạn rê chuột vào các tin nhắn hoặc ảnh đại diện trong cuộc trò chuyện."></span>
+              </label>
               <label class="zadark-switch__checkbox">
                 <input class="zadark-switch__input" type="checkbox" id="js-switch-hide-thread-chat-message">
                 <span class="zadark-switch__slider"></span>
@@ -463,4 +475,10 @@ const loadZaDarkPopup = () => {
 
   loadPopupState()
   loadKnownVersionState(buttonEl)
+
+  $('[data-tippy-content]').html(iconQuestionSVG)
+
+  tippy('[data-tippy-content]', {
+    theme: 'zadark'
+  })
 }
