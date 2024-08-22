bc28d547993b19d104ac6f18de406ea5b08d478f
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-08-22 23:48:48 +0700

    docs(CHANGELOG.md): update version to ZaDark 24.8.2 and PC 14.0 in changelog feat(package.json): update dependencies versions for @electron/asar and fs-extra feat(pc/index.js): add JSDoc comments for handleInstall and handleUninstall functions feat(pc/utils.js): add JSDoc comments for openWebsite, isFile, isDirectory, copyRecursiveAsync, copyDirectory, killProcesses functions
    refactor(zadark-pc.js): refactor code to improve readability and maintainability
    feat(zadark-pc.js): add new functions getZaloResDirList, getZaloProcessIds, removeZaDarkCSSAndJS, updateMetaContentSecurityPolicyTag, writeIndexFile, writeBootstrapFile, writeZNotificationFile, copyAssetDir to enhance Zalo PC customization capabilities
    fix(zadark-pc.js): update error messages with error codes for better identification and handling
    
    feat(zadark-pc.js): add functions installZaDark and uninstallZaDark to manage ZaDark theme installation and removal
    feat(zadark-pc.js): update installZaDark to handle directory checks and file operations
    feat(zadark-pc.js): update uninstallZaDark to handle directory checks and file operations

--- src/pc/utils.js
+++ src/pc/utils.js
@@ -1,15 +1,21 @@
 const chalk = require('chalk')
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const crossSpawn = require('cross-spawn')
+const { execSync } = require('child_process')
 
-const { IS_MAC } = require('./constants')
+const { IS_MAC, IS_WIN } = require('./constants')
 
 const print = (...args) => console.log(args.join(' '))
 const printDebug = (...args) => print(chalk.gray(...args))
 const printError = (...args) => console.log(chalk.redBright(args.join(' ')))
 const clearScreen = () => console.clear()
 
+/**
+ * Open a website in the default browser
+ * @param {string} url - The URL to open
+ * @returns {void}
+*/
 const openWebsite = (url) => {
   const cmd = IS_MAC ? 'open' : 'start'
   const _url = `"${url}"`
@@ -17,6 +23,50 @@ const openWebsite = (url) => {
   crossSpawn(cmd, args, { shell: true })
 }
 
+/**
+ * Check if the path is a file
+ * @param {string} p - The path to check
+ * @returns {boolean} - Returns true if the path is a file, otherwise false
+ * @throws {Error} - Throws error if the path is invalid
+ */
+const isFile = (p) => {
+  try {
+    const stats = fs.statSync(p)
+    return stats.isFile()
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false
+    } else {
+      throw error
+    }
+  }
+}
+
+/**
+ * Check if the path is a directory
+ * @param {string} p - The path to check
+ * @returns {boolean} - Returns true if the path is a directory, otherwise false
+ * @throws {Error} - Throws error if the path is invalid
+* */
+const isDirectory = (p) => {
+  try {
+    const stats = fs.statSync(p)
+    return stats.isDirectory()
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false
+    } else {
+      throw error
+    }
+  }
+}
+
+/**
+ * Copy a directory recursively
+ * @param {string} src - The source directory
+ * @param {string} dest - The destination directory
+ * @returns {void}
+ */
 const copyRecursiveSync = (src, dest) => {
   const exists = fs.existsSync(src)
   const stats = exists && fs.statSync(src)
@@ -46,6 +96,12 @@ const copyRecursiveSync = (src, dest) => {
 
 const fsPromises = fs.promises
 
+/**
+ * Copy a directory recursively asynchronously
+ * @param {string} src - The source directory
+ * @param {string} dest - The destination directory
+ * @returns {Promise<void>}
+ */
 const copyRecursiveAsync = async (src, dest) => {
   const stats = await fsPromises.stat(src)
   const isDirectory = stats.isDirectory()
@@ -69,6 +125,11 @@ const copyRecursiveAsync = async (src, dest) => {
   }
 }
 
+/**
+ * Kill a process by its PID
+ * @param {number} pid - The process ID
+ * @returns {boolean} - Returns true if the process was killed, otherwise false
+ */
 const killProcess = (pid) => {
   try {
     const args = IS_MAC ? [pid.toString()] : ['/F', '/PID', pid.toString()]
@@ -79,8 +140,36 @@ const killProcess = (pid) => {
   }
 }
 
+/**
+ * Kill multiple processes by their PIDs
+ * @param {number[]} processIds - An array of process IDs
+ * @returns {void}
+ */
 const killProcesses = (processIds = []) => {
   processIds.forEach((pid) => killProcess(pid))
+}
+
+/**
+ * Copy a directory to another directory
+ * @param {string} sourceDir - The source directory
+ * @param {string} destDir - The destination directory
+ * @returns {void}
+ * @throws {Error} - Throws error if the platform is not supported
+ */
+function copyDirectory (sourceDir, destDir) {
+  if (IS_WIN) {
+    // Windows: use xcopy command
+    execSync(`xcopy /E /I /Y "${sourceDir}" "${destDir}"`, { stdio: 'inherit' })
+    return
+  }
+
+  if (IS_MAC) {
+    // macOS: use ditto command
+    execSync(`ditto "${sourceDir}" "${destDir}"`, { stdio: 'inherit' })
+    return
+  }
+
+  throw new Error('Unsupported platform')
 }
 
 module.exports = {
@@ -90,8 +179,13 @@ module.exports = {
   clearScreen,
 
   openWebsite,
+
+  isFile,
+  isDirectory,
+
   copyRecursiveSync,
   copyRecursiveAsync,
+  copyDirectory,
 
   killProcess,
   killProcesses
--- src/pc/zadark-pc.js
+++ src/pc/zadark-pc.js
@@ -1,18 +1,26 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const del = require('del')
 const asar = require('@electron/asar')
 const HTMLParser = require('node-html-parser')
 const glob = require('glob')
 
-const { printDebug, printError, copyRecursiveAsync } = require('./utils')
+const { printDebug, printError, copyRecursiveAsync, copyDirectory, isDirectory, isFile } = require('./utils')
 
 const { PLATFORM, IS_MAC, OS_NAME, ZADARK_VERSION, ZADARK_TMP_PATH, ZALO_PROCESS_NAMES, ZADARK_API_DOMAIN } = require('./constants')
+
 const psList = require('./packages/ps-list')
 
+/**
+ * Get Zalo resources directory list
+ * @param {string} customZaloPath - The custom Zalo path
+ * @returns {string[]} - The Zalo resources directory list
+ * @throws {Error} - Throws error if the platform is not supported
+ * @throws {Error} - Throws error if the resources directory list is empty
+ */
 const getZaloResDirList = (customZaloPath) => {
   if (!['darwin', 'win32'].includes(PLATFORM)) {
-    throw new Error(`Khong ho tro he dieu hanh "${PLATFORM}".`)
+    throw new Error(`Khong ho tro he dieu hanh "${PLATFORM}" (E006).`)
   }
 
   const resourcesPath = IS_MAC
@@ -28,6 +36,11 @@ const getZaloResDirList = (customZaloPath) => {
   return resources.sort()
 }
 
+/**
+ * Get Zalo process IDs
+ * @returns {Promise<number[]>} - The Zalo process IDs
+ * @throws {Error} - Throws error if the Zalo process names are not found
+ */
 const getZaloProcessIds = async () => {
   try {
     const processes = await psList()
@@ -38,10 +51,20 @@ const getZaloProcessIds = async () => {
 
     return pids
   } catch (error) {
-    throw new Error('Vui long tat Zalo PC truoc khi cai dat ZaDark.')
+    throw new Error('Vui long tat Zalo PC truoc khi cai dat ZaDark (E007).')
   }
 }
 
+/**
+ * Removes all CSS and JavaScript files from the document that have filenames starting with "zadark".
+ *
+ * This function searches for `<link>` elements in the `headElement` and `<script>` elements in the
+ * `bodyElement` whose `href` or `src` attributes start with "zadark", and removes them from the DOM.
+ *
+ * @param {Object} params - The parameters for the function.
+ * @param {HTMLElement} params.headElement - The `<head>` element of the document, used to search for `<link>` elements.
+ * @param {HTMLElement} params.bodyElement - The `<body>` element of the document, used to search for `<script>` elements.
+ */
 const removeZaDarkCSSAndJS = ({ headElement, bodyElement }) => {
   const elementsToRemove = [
     {
@@ -62,6 +85,20 @@ const removeZaDarkCSSAndJS = ({ headElement, bodyElement }) => {
   })
 }
 
+/**
+ * Updates the Content-Security-Policy (CSP) meta tag in the provided HTML element.
+ *
+ * This function searches for a `<meta>` tag with the `http-equiv="Content-Security-Policy"` attribute
+ * in the specified `htmlElement`. If the meta tag is found, it updates its `content` attribute by:
+ * - Adding `https://fonts.googleapis.com` to the `style-src` directive if it's not already present.
+ * - Adding `ZADARK_API_DOMAIN` to the `connect-src` directive if it's not already present.
+ *
+ * If the meta tag is not found, the function logs an error message and skips the update.
+ *
+ * @param {HTMLElement} htmlElement - The HTML element to search for the Content-Security-Policy meta tag.
+ *
+ * @throws Will throw an error if `ZADARK_API_DOMAIN` is not defined in the scope.
+ */
 const updateMetaContentSecurityPolicyTag = (htmlElement) => {
   const metaTag = htmlElement.querySelector('meta[http-equiv="Content-Security-Policy"]')
 
@@ -86,12 +123,29 @@ const updateMetaContentSecurityPolicyTag = (htmlElement) => {
   metaTag.setAttribute('content', contentValue)
 }
 
+/**
+ * Modifies the `index.html` file located in the specified Zalo directory by adding necessary CSS,
+ * JavaScript files, and attributes required for the ZaDark theme.
+ *
+ * The function performs the following tasks:
+ * 1. Verifies the existence of the `index.html` file in the `zaloDir`.
+ * 2. Parses the HTML and removes existing ZaDark-related CSS and JS files.
+ * 3. Adds the required CSS and JS files to the `<head>` and `<body>` elements.
+ * 4. Updates the Content-Security-Policy meta tag with necessary sources.
+ * 5. Adds required data attributes and class names to the `<html>` and `<body>` elements.
+ * 6. Saves the modified HTML content back to the original file.
+ *
+ * @param {string} zaloDir - The directory where the Zalo application is located.
+ *                           This is the base path for the `index.html` file.
+ *
+ * @throws {Error} If the `index.html` file does not exist at the expected path.
+ */
 const writeIndexFile = (zaloDir) => {
   const src = 'pc-dist/index.html'
   const srcPath = path.join(zaloDir, `app/${src}`)
 
-  if (!fs.existsSync(srcPath)) {
-    throw new Error(srcPath + ' khong ton tai.')
+  if (!isFile(srcPath)) {
+    throw new Error(srcPath + ' khong ton tai (E005).')
   }
 
   const indexHTMLContent = fs.readFileSync(srcPath, 'utf8')
@@ -219,11 +273,24 @@ const writeIndexFile = (zaloDir) => {
   fs.writeFileSync(srcPath, root.toString())
 }
 
+/**
+ * Modifies the `bootstrap.js` file located in the specified Zalo directory by inserting a
+ * required `zadark-main.min` module if it doesn't already exist.
+ *
+ * The function performs the following tasks:
+ * 1. Verifies the existence of the `bootstrap.js` file in the `zaloDir`.
+ * 2. Checks if the `zadark-main.min` module is already required in the file.
+ * 3. If not, inserts the require statement after the `main-dist/main` module.
+ * 4. Saves the modified content back to the original file.
+ *
+ * @param {string} zaloDir - The directory where the Zalo application is located.
+ *                           This is the base path for the `bootstrap.js` file.
+ */
 const writeBootstrapFile = (zaloDir) => {
   const src = 'bootstrap.js'
   const srcPath = path.join(zaloDir, `app/${src}`)
 
-  if (!fs.existsSync(srcPath)) {
+  if (!isFile(srcPath)) {
     printError('- writeFile:', srcPath, 'skip: file does not exist.')
     return
   }
@@ -244,11 +311,25 @@ const writeBootstrapFile = (zaloDir) => {
   fs.writeFileSync(srcPath, insertedContent)
 }
 
+/**
+ * Modifies the `znotification.html` file located in the specified Zalo directory by adding necessary
+ * CSS, JavaScript files, and class names required for the ZaDark theme.
+ *
+ * The function performs the following tasks:
+ * 1. Verifies the existence of the `znotification.html` file in the `zaloDir`.
+ * 2. Parses the HTML and removes existing ZaDark-related CSS and JS files.
+ * 3. Adds the required CSS and JS files to the `<head>` and `<body>` elements.
+ * 4. Adds required class names to the `<body>` element.
+ * 5. Saves the modified HTML content back to the original file.
+ *
+ * @param {string} zaloDir - The directory where the Zalo application is located.
+ *                           This is the base path for the `znotification.html` file.
+ */
 const writeZNotificationFile = (zaloDir) => {
   const src = 'pc-dist/znotification.html'
   const srcPath = path.join(zaloDir, `app/${src}`)
 
-  if (!fs.existsSync(srcPath)) {
+  if (!isFile(srcPath)) {
     printError('- writeFile:', srcPath, 'skip: file does not exist.')
     return
   }
@@ -300,12 +381,28 @@ const writeZNotificationFile = (zaloDir) => {
   fs.writeFileSync(srcPath, root.toString())
 }
 
+/**
+ * Asynchronously copies the contents of a source asset directory to a destination directory within the specified Zalo directory.
+ *
+ * The function performs the following tasks:
+ * 1. Verifies the existence of the source asset directory.
+ * 2. Copies the contents of the source directory to the destination directory.
+ *
+ * @param {string} zaloDir - The directory where the Zalo application is located. This is the base path for the destination directory.
+ * @param {Object} options - Options for the source and destination paths.
+ * @param {string} options.dest - The relative path of the destination directory within the Zalo application directory.
+ * @param {string} options.src - The relative path of the source asset directory to be copied.
+ *
+ * @throws {Error} If the source asset directory does not exist.
+ *
+ * @returns {Promise<void>} A promise that resolves when the copying process is complete.
+ */
 const copyAssetDir = async (zaloDir, { dest, src }) => {
   const srcPath = path.join(__dirname, `./assets/${src}`)
   const destPath = path.join(zaloDir, `app/${dest}`)
 
   if (!fs.existsSync(srcPath)) {
-    throw new Error(srcPath + ' khong ton tai.')
+    throw new Error(srcPath + ' khong ton tai (E004).')
   }
 
   printDebug('- copyDir:', src, '>', destPath)
@@ -347,47 +444,69 @@ const copyAssets = async () => {
   }
 }
 
+/**
+ * Installs ZaDark by updating the `app.asar` file in the specified Zalo directory with the ZaDark theme.
+ *
+ * The function performs the following tasks:
+ * 1. Verifies that the given directory exists and is valid.
+ * 2. Deletes the existing `app.asar` file if it is a directory and the temporary directory `ZADARK_TMP_PATH` if it exists.
+ * 3. Creates a new temporary directory `ZADARK_TMP_PATH` and extracts the `app.asar` file into it.
+ * 4. Copies necessary assets and modifies specific files (`index.html`, `bootstrap.js`, and optionally `znotification.html`) within the extracted content.
+ * 5. Renames the original `app.asar` file to `app.asar.bak` if it does not already exist.
+ * 6. Replaces the `app.asar` file with the updated content from the temporary directory.
+ * 7. Cleans up by deleting the temporary directory `ZADARK_TMP_PATH`.
+ *
+ * @param {string} zaloDir - The directory where the Zalo application is located.
+ *
+ * @throws {Error} If the specified directory does not exist or if required files are missing.
+ *
+ * @returns {Promise<void>} A promise that resolves when the installation process is complete.
+ */
 const installZaDark = async (zaloDir) => {
-  if (!fs.existsSync(zaloDir)) {
-    throw new Error(zaloDir + ' khong ton tai.')
+  if (!isDirectory(zaloDir)) {
+    throw new Error(zaloDir + ' khong ton tai (E001).')
   }
 
   const appAsarPath = path.join(zaloDir, 'app.asar')
   const appAsarBakPath = path.join(zaloDir, 'app.asar.bak')
-
   const appDirTmpPath = path.join(ZADARK_TMP_PATH, 'app')
-  const appAsarTmpPath = path.join(ZADARK_TMP_PATH, 'app.asar')
 
-  if (!fs.existsSync(appAsarPath)) {
-    throw new Error(zaloDir + ' khong co tap tin "app.asar".')
+  if (!isFile(appAsarPath)) {
+    throw new Error(zaloDir + ' khong co tap tin "app.asar" (E002).')
   }
 
-  // Delete ZADARK_TMP_PATH
-  if (fs.existsSync(ZADARK_TMP_PATH)) {
+  // Delete dir "resources/app.asar"
+  if (isDirectory(appAsarPath)) {
+    printDebug('- deleteDir:', appAsarPath)
+    await del(appAsarPath, { force: true })
+  }
+
+  // Delete dir "ZADARK_TMP_PATH"
+  if (isDirectory(ZADARK_TMP_PATH)) {
     printDebug('- deleteDir:', ZADARK_TMP_PATH)
     await del(ZADARK_TMP_PATH, { force: true })
   }
 
+  // Create dir "ZADARK_TMP_PATH"
   printDebug('- createDir:', ZADARK_TMP_PATH)
   fs.mkdirSync(ZADARK_TMP_PATH)
 
-  // Extract "resources/app.asar" to "ZADARK_TMP_PATH/app"
+  // Extract file "resources/app.asar" to dir "ZADARK_TMP_PATH/app"
   printDebug('- extractAsar:', appAsarPath, '>', appDirTmpPath)
   asar.extractAll(appAsarPath, appDirTmpPath)
 
-  // Copy assets
   await copyAssets()
 
   const writeFilePromises = [
-    // Add "themeAttributes, classNames, fonts, stylesheets and scripts" to "ZADARK_TMP_PATH/app/pc-dist/index.html"
+    // Inject "themeAttributes, classNames, fonts, stylesheets and scripts" to file "ZADARK_TMP_PATH/app/pc-dist/index.html"
     writeIndexFile(ZADARK_TMP_PATH),
 
-    // Add zadark-main to "ZADARK_TMP_PATH/app/bootstrap.js"
+    // Inject zadark-main to file "ZADARK_TMP_PATH/app/bootstrap.js"
     writeBootstrapFile(ZADARK_TMP_PATH)
   ]
 
   if (!IS_MAC) {
-    // Add fonts, stylesheets and scripts" to "ZADARK_TMP_PATH/app/pc-dist/znotification.html"
+    // Inject fonts, stylesheets and scripts" to "ZADARK_TMP_PATH/app/pc-dist/znotification.html"
     writeFilePromises.push(
       writeZNotificationFile(ZADARK_TMP_PATH)
     )
@@ -395,34 +514,51 @@ const installZaDark = async (zaloDir) => {
 
   await Promise.all(writeFilePromises)
 
-  // Create package "ZADARK_TMP_PATH/app.asar" from "ZADARK_TMP_PATH/app"
-  printDebug('- createAsar:', appDirTmpPath, '>', appAsarTmpPath)
-  await asar.createPackage(appDirTmpPath, appAsarTmpPath)
-
-  // Rename "resources/app.asar" to "resources/app.asar.bak"
-  if (!fs.existsSync(appAsarBakPath)) {
+  // Rename file "resources/app.asar" to "resources/app.asar.bak"
+  if (!isFile(appAsarBakPath)) {
     printDebug('- renameFile:', appAsarPath, '>', appAsarBakPath)
     fs.renameSync(appAsarPath, appAsarBakPath)
   }
 
-  // Copy "ZADARK_TMP_PATH/app.asar" to "resources/app.asar"
-  printDebug('- copyFile:', appAsarTmpPath, '>', appAsarPath)
-  fs.copyFileSync(appAsarTmpPath, appAsarPath)
+  // Copy dir "ZADARK_TMP_PATH/app" to dir "resources/app.asar"
+  printDebug('- copyDir:', appDirTmpPath, '>', appAsarPath)
+  copyDirectory(appDirTmpPath, appAsarPath)
 
-  // Delete "ZADARK_TMP_PATH"
+  // Delete dir "ZADARK_TMP_PATH"
   printDebug('- deleteDir:', ZADARK_TMP_PATH)
   await del(ZADARK_TMP_PATH, { force: true })
 }
 
+/**
+ * Uninstalls ZaDark by restoring the original `app.asar` file in the specified Zalo directory.
+ *
+ * The function performs the following tasks:
+ * 1. Verifies the existence of the backup `app.asar.bak` file.
+ * 2. Deletes the existing `app.asar` directory if it is a directory.
+ * 3. Renames the backup file `app.asar.bak` to `app.asar` to restore the original state.
+ *
+ * @param {string} zaloDir - The directory where the Zalo application is located.
+ *
+ * @throws {Error} If the backup file `app.asar.bak` does not exist, indicating that ZaDark installation was not successful.
+ *
+ * @returns {Promise<void>} A promise that resolves when the uninstallation process is complete.
+ */
 const uninstallZaDark = async (zaloDir) => {
+  const appDirPath = path.join(zaloDir, 'app')
   const appAsarPath = path.join(zaloDir, 'app.asar')
   const appAsarBakPath = path.join(zaloDir, 'app.asar.bak')
 
-  if (!fs.existsSync(appAsarBakPath)) {
-    throw new Error('Go cai dat ZaDark khong thanh cong.')
+  if (!isFile(appAsarBakPath)) {
+    throw new Error('Go cai dat ZaDark khong thanh cong (E003).')
   }
 
-  // Rename "resources/app.asar.bak" to "resources/app.asar"
+  // Delete dir "resources/app.asar"
+  if (isDirectory(appAsarPath)) {
+    printDebug('- deleteDir:', appDirPath)
+    await del(appAsarPath, { force: true })
+  }
+
+  // Rename file "resources/app.asar.bak" to "resources/app.asar"
   printDebug('- renameFile:', appAsarBakPath, '>', appAsarPath)
   fs.renameSync(appAsarBakPath, appAsarPath)
 }
